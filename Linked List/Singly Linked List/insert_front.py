0b88efec51efbaff28dba387fb9168c5e5d967ea
Keerthan M P <keerthankeeru775@gmail.com>
2022-04-18 22:14:11 +0530

    sll insert front

---
+++ Linked List/Singly Linked List/insert_front.py
@@ -0,0 +1,16 @@
+from linked_list import Node, get_list
+
+def insert_front(l):
+    if not l:
+        print("No data")
+        return []
+    elif len(l) == 1:
+        return Node(l[0])
+    else:
+        linked_list = Node(l[0])
+        temp = linked_list
+        for i in l[1:]:
+            node = Node(i)
+            temp.next = node
+            temp = temp.next
+        return linked_list
